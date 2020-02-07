1a894145e337278965b4cee386841f1f4412ca89
zhong <56583406+zchubin@users.noreply.github.com>
2020-02-07 20:29:36 +0800

    Add files via upload

---
+++ Java/App3_1.java
@@ -0,0 +1,11 @@
+//filename:App3_1.java
+public class App3_1     //定义类App3_1
+{
+    public static void main(String[] args)
+    {
+        int a = 155;
+        float b = 21.0f;
+        System.out.println("a="+a+",b="+b); //输出a,b的值;
+        System.out.println("a/b="+(a/b)); //输出a/b的值;
+    }
+}
